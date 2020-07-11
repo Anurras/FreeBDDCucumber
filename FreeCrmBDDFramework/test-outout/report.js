b3b57a284eddfd018ab348e79a6c53a327e6bc42
Anurras <anurrastogi91@gmail.com>
2020-07-11 21:30:26 +0530

    commit

---
+++ FreeCrmBDDFramework/test-outout/report.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Free CRM Login Feature",
+  "description": "",
+  "id": "free-crm-login-feature",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Free CRM Login Test Scenario",
+  "description": "",
+  "id": "free-crm-login-feature;free-crm-login-test-scenario",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "user is already on Login Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "title of login page is Free CRM",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "user enters username and password",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "user clicks on login button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user is on home page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_already_on_Login_Page()"
+});
+formatter.result({
+  "duration": 297977800,
+  "error_message": "java.lang.IllegalStateException: The driver executable does not exist: C:\\Users\\Anur Rastogi\\eclipse-workspace\\FreeCrmBDDFramework\\‪F:\\Driver\\chromedriver\r\n\tat com.google.common.base.Preconditions.checkState(Preconditions.java:585)\r\n\tat org.openqa.selenium.remote.service.DriverService.checkExecutable(DriverService.java:146)\r\n\tat org.openqa.selenium.remote.service.DriverService.findExecutable(DriverService.java:141)\r\n\tat org.openqa.selenium.chrome.ChromeDriverService.access$000(ChromeDriverService.java:35)\r\n\tat org.openqa.selenium.chrome.ChromeDriverService$Builder.findDefaultExecutable(ChromeDriverService.java:159)\r\n\tat org.openqa.selenium.remote.service.DriverService$Builder.build(DriverService.java:355)\r\n\tat org.openqa.selenium.chrome.ChromeDriverService.createDefaultService(ChromeDriverService.java:94)\r\n\tat org.openqa.selenium.chrome.ChromeDriver.\u003cinit\u003e(ChromeDriver.java:123)\r\n\tat stepDefinitions.LoginStepDefinition.user_is_already_on_Login_Page(LoginStepDefinition.java:18)\r\n\tat ✽.Given user is already on Login Page(login.feature:5)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.title_of_login_page_is_free_CRM()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_enters_username_and_password(String,String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_clicks_on_login_button()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginStepDefinition.user_is_on_hopme_page()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
